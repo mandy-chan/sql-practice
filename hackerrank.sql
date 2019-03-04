bec9b12eb3ffd24ac9d390129c082deba952e543
Mandy <41100622+mandy-chan@users.noreply.github.com>
2019-03-03 19:41:22 -0800

    Update hackerrank.sql

--- hackerrank.sql
+++ hackerrank.sql
@@ -1,13 +1,10 @@
-/* 
-
-Query an alphabetically ordered list of all names in OCCUPATIONS, immediately followed by the first letter of each profession as a parenthetical (i.e.: enclosed in parentheses). For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and ASingerName(S).
+/* Query an alphabetically ordered list of all names in OCCUPATIONS, immediately followed by the first letter of each profession as a parenthetical (i.e.: enclosed in parentheses). For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and ASingerName(S).
 Query the number of ocurrences of each occupation in OCCUPATIONS. Sort the occurrences in ascending order, and output them in the following format:
 
 There are a total of [occupation_count] [occupation]s.
 
 where [occupation_count] is the number of occurrences of an occupation in OCCUPATIONS and [occupation] is the lowercase occupation name. If more than one Occupation has the same [occupation_count], they should be ordered alphabetically.
 Note: There will be at least two entries in the table for each type of occupation. */
-
 
 SELECT CONCAT(o.Name, '(', SUBSTRING(o.Occupation,1, 1), ')')
 FROM occupations o
@@ -20,4 +17,6 @@ SELECT 'There are a total of', COUNT(o.Occupation), CONCAT(LOWER(o.Occupation),'
 FROM occupations o
 GROUP BY o.Occupation
 ORDER BY COUNT(o.Occupation), o.Occupation;
+
+
 
